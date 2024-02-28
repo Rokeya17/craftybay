5de3ddfa45d3fd69f3fa5c6b5694a682021f390d
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-02-28 20:40:21 +0600

    email_verification added

--- lib/presentation/ui/screens/auth/email_verification.dart
+++ lib/presentation/ui/screens/auth/email_verification.dart
@@ -15,29 +15,43 @@ class _EmailVerificationState extends State<EmailVerification> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            Center(
-              child: SvgPicture.asset(
-                ImageAssets.craftybaylogoSVG,
-                width: 120,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
               ),
-            ),
-            Text('Welcome Back',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge
-                    ?.copyWith(fontSize: 24)),
-            Text(
-              'Please enter your email address',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-            const TextField(),
-            ElevatedButton(onPressed: () {}, child: const Text('Next'))
-          ],
+              Center(
+                child: SvgPicture.asset(
+                  ImageAssets.craftybaylogoSVG,
+                  width: 120,
+                ),
+              ),
+              const SizedBox(width: 16),
+              Text('Welcome Back',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(fontSize: 24, color: Colors.black)),
+              const SizedBox(width: 16),
+              Text('Please enter your email address',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleSmall
+                      ?.copyWith(color: Colors.blueGrey)),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(),
+                  disabledBorder: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(onPressed: () {}, child: const Text('Next'))
+            ],
+          ),
         ),
       ),
     );
