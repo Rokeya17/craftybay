7f0a860cc3683f12e9df33366e9ba47cc70d7292
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-01 15:25:17 +0600

    complete profile screen ui design added

--- lib/presentation/ui/screens/auth/complete_profile_screen.dart
+++ lib/presentation/ui/screens/auth/complete_profile_screen.dart
@@ -1,16 +1,15 @@
+import 'package:craftybay/presentation/utility/image_assets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../../utility/image_assets.dart';
-
-class CompleteProfileScreen extends StatefulWidget {
-  const CompleteProfileScreen({super.key});
+class CompleteProfile extends StatefulWidget {
+  const CompleteProfile({super.key});
 
   @override
-  State<CompleteProfileScreen> createState() => _CompleteProfileScreenState();
+  State<CompleteProfile> createState() => _CompleteProfileState();
 }
 
-class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
+class _CompleteProfileState extends State<CompleteProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,39 +19,58 @@ class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
+                const SizedBox(
+                  height: 100,
+                ),
                 Center(
                   child: SvgPicture.asset(
                     ImageAssets.craftybaylogoSVG,
                     width: 120,
                   ),
                 ),
-                const Spacer(),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text('Complete Profile',
+                const SizedBox(width: 16),
+                Text('Complete Profile ',
                     style: Theme.of(context)
                         .textTheme
                         .titleLarge
                         ?.copyWith(fontSize: 24, color: Colors.black)),
                 const SizedBox(height: 8),
-                Text('Get started with us with your detail',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleSmall
-                        ?.copyWith(fontSize: 12, color: Colors.grey)),
-                const SizedBox(height: 16),
-                TextFormField(),
-                const SizedBox(height: 16),
-                TextFormField(),
-                const SizedBox(height: 16),
-                TextFormField(),
-                const SizedBox(height: 16),
-                TextFormField(),
+                Text(
+                  'Get started with us with your details',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleSmall
+                      ?.copyWith(fontSize: 12, color: Colors.grey),
+                ),
                 const SizedBox(height: 16),
                 TextFormField(
-                  style: const TextStyle(
-                    height: 7,
+                  decoration: const InputDecoration(
+                    labelText: 'First Name',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Last Name',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Mobile',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'City',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  maxLines: 6,
+                  decoration: const InputDecoration(
+                    labelText: 'Shipping Address',
                   ),
                 ),
                 const SizedBox(height: 16),
--- lib/presentation/ui/screens/auth/otp_verification.dart
+++ lib/presentation/ui/screens/auth/otp_verification.dart
@@ -18,87 +18,89 @@ class _OTPVerificationState extends State<OTPVerification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              Center(
-                child: SvgPicture.asset(
-                  ImageAssets.craftybaylogoSVG,
-                  width: 120,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 100,
                 ),
-              ),
-              const SizedBox(width: 16),
-              Text('Enter your OTP code',
+                Center(
+                  child: SvgPicture.asset(
+                    ImageAssets.craftybaylogoSVG,
+                    width: 120,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Text('Enter your OTP code',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(fontSize: 24, color: Colors.black)),
+                const SizedBox(height: 8),
+                Text(
+                  'A 4 digit OTP code has been send',
                   style: Theme.of(context)
                       .textTheme
-                      .titleLarge
-                      ?.copyWith(fontSize: 24, color: Colors.black)),
-              const SizedBox(height: 8),
-              Text(
-                'A 4 digit OTP code has been send',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleSmall
-                    ?.copyWith(fontSize: 12, color: Colors.grey),
-              ),
-              const SizedBox(height: 16),
-              SingleChildScrollView(
-                child: PinCodeTextField(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  appContext: context,
-                  length: 4,
-                  obscureText: false,
-                  animationType: AnimationType.slide,
-                  pinTheme: PinTheme(
-                    shape: PinCodeFieldShape.box,
-                    borderRadius: BorderRadius.circular(5),
-                    fieldHeight: 50,
-                    fieldWidth: 40,
-                    activeFillColor: Colors.white,
-                    inactiveFillColor: Colors.white,
-                    selectedFillColor: AppColors.primaryColor,
-                  ),
-                  animationDuration: const Duration(milliseconds: 120),
-                  backgroundColor: Colors.white,
-                  enableActiveFill: true,
-                  onCompleted: (v) {},
-                  onChanged: (value) {
-                    setState(() {});
-                  },
-                  beforeTextPaste: (text) {
-                    return true;
-                  },
+                      .titleSmall
+                      ?.copyWith(fontSize: 12, color: Colors.grey),
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: () {
-                      Get.to(const CompleteProfileScreen());
+                const SizedBox(height: 16),
+                SingleChildScrollView(
+                  child: PinCodeTextField(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    appContext: context,
+                    length: 4,
+                    obscureText: false,
+                    animationType: AnimationType.slide,
+                    pinTheme: PinTheme(
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(5),
+                      fieldHeight: 50,
+                      fieldWidth: 40,
+                      activeFillColor: Colors.white,
+                      inactiveFillColor: Colors.white,
+                      selectedFillColor: AppColors.primaryColor,
+                    ),
+                    animationDuration: const Duration(milliseconds: 120),
+                    backgroundColor: Colors.white,
+                    enableActiveFill: true,
+                    onCompleted: (v) {},
+                    onChanged: (value) {
+                      setState(() {});
                     },
-                    child: const Text('Next')),
-              ),
-              const SizedBox(height: 8),
-              RichText(
-                text: const TextSpan(
-                  style: TextStyle(color: Colors.grey),
-                  children: [
-                    TextSpan(text: "Didn't receive the code?"),
-                  ],
+                    beforeTextPaste: (text) {
+                      return true;
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(
+                SizedBox(
                   width: double.infinity,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text('Resend'),
-                  )),
-            ],
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Get.offAll(const CompleteProfile());
+                      },
+                      child: const Text('Next')),
+                ),
+                const SizedBox(height: 8),
+                RichText(
+                  text: const TextSpan(
+                    style: TextStyle(color: Colors.grey),
+                    children: [
+                      TextSpan(text: "Didn't receive the code?"),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text('Resend'),
+                    )),
+              ],
+            ),
           ),
         ),
       ),
