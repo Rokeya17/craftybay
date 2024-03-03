e1d479180209a2492e0ab1177c5d86c5ea2542b5
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-03 21:15:12 +0600

    CarouselSlider added

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:craftybay/presentation/utility/image_assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -36,16 +37,58 @@ class HomeScreen extends StatelessWidget {
           ),
         ]),
       ),
-      body: const SingleChildScrollView(
-        child: Column(children: [
-          TextField(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(children: [
+            const TextField(
               decoration: InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
+                  prefixIcon: Icon(
+                    Icons.search,
+                  ),
+                  hintText: 'Search',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  )),
             ),
-          )),
-        ]),
+            const SizedBox(
+              height: 16,
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 200.0,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3)),
+              items: [1, 2, 3].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: const BoxDecoration(color: Colors.blue),
+                        child: Text(
+                          'text $i',
+                          style: const TextStyle(fontSize: 16.0),
+                        ));
+                  },
+                );
+              }).toList(),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 10,
+              width: 10,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey)),
+            )
+          ]),
+        ),
       ),
     );
   }
