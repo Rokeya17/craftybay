48762b6d242c4db4fad890f4eed16f0882f658ce
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-03 22:45:28 +0600

    homeslider added

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:craftybay/presentation/utility/app_colors.dart';
 import 'package:craftybay/presentation/utility/image_assets.dart';
+import 'package:craftybay/presentation/widgets/home/homeslider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -14,7 +13,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final ValueNotifier<int> _selectedSlider = ValueNotifier(0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,12 +42,12 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ]),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(16.0),
           child: Center(
             child: Column(children: [
-              const TextField(
+              TextField(
                 decoration: InputDecoration(
                     prefixIcon: Icon(
                       Icons.search,
@@ -62,60 +60,13 @@ class _HomeScreenState extends State<HomeScreen> {
                       borderSide: BorderSide.none,
                     )),
               ),
-              const SizedBox(
+              SizedBox(
                 height: 16,
               ),
-              CarouselSlider(
-                options: CarouselOptions(
-                    height: 200.0,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 3),
-                    onPageChanged: (int page, _) {
-                      _selectedSlider.value = page;
-                    }),
-                items: [1, 2, 3].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                          decoration: const BoxDecoration(color: Colors.blue),
-                          child: Text(
-                            'text $i',
-                            style: const TextStyle(fontSize: 16.0),
-                          ));
-                    },
-                  );
-                }).toList(),
-              ),
-              const SizedBox(
+              HomeSlider(),
+              SizedBox(
                 height: 16,
               ),
-              ValueListenableBuilder(
-                  valueListenable: _selectedSlider,
-                  builder: (context, value, _) {
-                    List<Widget> list = [];
-
-                    for (int i = 0; i < 3; i++) {
-                      list.add(
-                        Container(
-                          height: 10,
-                          width: 10,
-                          margin: const EdgeInsets.symmetric(horizontal: 1.5),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey),
-                              borderRadius: BorderRadius.circular(10),
-                              color:
-                                  value == i ? AppColors.primaryColor : null),
-                        ),
-                      );
-                    }
-
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: list,
-                    );
-                  }),
             ]),
           ),
         ),
