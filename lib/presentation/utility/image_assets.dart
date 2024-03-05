ebcdc479b2010b32711d5cf7097117137ea01893
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-05 15:47:57 +0600

    Home screen

--- lib/presentation/utility/image_assets.dart
+++ lib/presentation/utility/image_assets.dart
@@ -1,4 +1,5 @@
 class ImageAssets {
   static const String craftybaylogoSVG = 'assets/images/logo.svg';
   static const String craftybaylogonavSVG = 'assets/images/logo_nav.svg';
+  static const String shoepng = 'assets/images/shoe.png';
 }
