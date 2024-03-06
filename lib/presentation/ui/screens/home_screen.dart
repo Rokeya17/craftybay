a9c70f5c3cd656e61636e230354256df3dbce4b8
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-06 16:03:04 +0600

    Product card added

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:craftybay/presentation/utility/app_colors.dart';
 import 'package:craftybay/presentation/widgets/category_card.dart';
 import 'package:craftybay/presentation/widgets/home/homeslider.dart';
 import 'package:craftybay/presentation/widgets/home/section_header.dart';
@@ -7,6 +6,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../utility/image_assets.dart';
 import '../../widgets/circular_iconbutton.dart';
+import '../../widgets/home/product_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -84,61 +84,12 @@ class _HomeScreenState extends State<HomeScreen> {
                 onTap: () {},
                 title: 'Popular',
               ),
-              Card(
-                shadowColor: AppColors.primaryColor.withOpacity(0.1),
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-                child: SizedBox(
-                  width: 100,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 100,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8)),
-                          image: const DecorationImage(
-                              image: AssetImage(
-                            ImageAssets.shoepng,
-                          )),
-                          color: AppColors.primaryColor.withOpacity(0.1),
-                        ),
-                      ),
-                      const Text(
-                        'Nike shoes Ak10203040',
-                        maxLines: 1,
-                        style: TextStyle(overflow: TextOverflow.ellipsis),
-                      ),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('\$90'),
-                          Wrap(children: [
-                            Icon(
-                              Icons.star_border,
-                              color: Colors.amber,
-                              size: 15,
-                            ),
-                            Text('90'),
-                          ]),
-                          Card(
-                            color: Colors.red,
-                            child: Padding(
-                              padding: EdgeInsets.all(2.0),
-                              child: Icon(
-                                Icons.favorite_border,
-                                size: 15,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              )
+              ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return const ProductCard();
+                  }),
             ]),
           ),
         ),
---
+++ lib/presentation/widgets/home/product_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import '../../utility/app_colors.dart';
+import '../../utility/image_assets.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shadowColor: AppColors.primaryColor.withOpacity(0.1),
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: SizedBox(
+        width: 120,
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+                image: const DecorationImage(
+                    image: AssetImage(
+                  ImageAssets.shoepng,
+                )),
+                color: AppColors.primaryColor.withOpacity(0.1),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            const Text(
+              'Nike shoes Ak10203040',
+              maxLines: 1,
+              style: TextStyle(
+                  overflow: TextOverflow.ellipsis, fontWeight: FontWeight.w500),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '\$90',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.primaryColor),
+                ),
+                Wrap(crossAxisAlignment: WrapCrossAlignment.center, children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: 20,
+                  ),
+                  Text(
+                    '4.5',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ]),
+                Card(
+                  color: Colors.red,
+                  child: Padding(
+                    padding: EdgeInsets.all(2.0),
+                    child: Icon(
+                      Icons.favorite_border,
+                      size: 16,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
