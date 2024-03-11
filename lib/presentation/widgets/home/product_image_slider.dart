6a96d48c9a743689357071e50cc89042a9910230
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-11 23:58:50 +0600

    product image slider  UI screen added

--- lib/presentation/widgets/home/product_image_slider.dart
+++ lib/presentation/widgets/home/product_image_slider.dart
@@ -13,56 +13,62 @@ class _ProductImageSliderState extends State<ProductImageSlider> {
   final ValueNotifier<int> _selectedSlider = ValueNotifier(0);
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        CarouselSlider(
-          options: CarouselOptions(
-              height: 260.0,
-              autoPlay: true,
-              viewportFraction: 1,
-              onPageChanged: (int page, _) {
-                _selectedSlider.value = page;
-              }),
-          items: [1, 2, 3].map((i) {
-            return Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(color: Colors.grey),
-                );
-              },
-            );
-          }).toList(),
-        ),
-        Positioned(
-          bottom: 10,
-          left: 0,
-          right: 0,
-          child: ValueListenableBuilder(
-              valueListenable: _selectedSlider,
-              builder: (context, value, _) {
-                List<Widget> list = [];
-                for (int i = 0; i < 3; i++) {
-                  list.add(
-                    Container(
-                      height: 10,
-                      width: 10,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: value == i
-                              ? AppColors.primaryColor
-                              : Colors.white),
-                    ),
+    return SafeArea(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                    height: 260.0,
+                    autoPlay: true,
+                    viewportFraction: 1,
+                    onPageChanged: (int page, _) {
+                      _selectedSlider.value = page;
+                    }),
+                items: [1, 2, 3].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(color: Colors.grey),
+                      );
+                    },
                   );
-                }
+                }).toList(),
+              ),
+              Positioned(
+                bottom: 10,
+                left: 0,
+                right: 0,
+                child: ValueListenableBuilder(
+                    valueListenable: _selectedSlider,
+                    builder: (context, value, _) {
+                      List<Widget> list = [];
+                      for (int i = 0; i < 3; i++) {
+                        list.add(
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: value == i
+                                    ? AppColors.primaryColor
+                                    : Colors.white),
+                          ),
+                        );
+                      }
 
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: list,
-                );
-              }),
-        ),
-      ],
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: list,
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
