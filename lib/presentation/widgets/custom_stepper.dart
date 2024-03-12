23298e4f6f88920780934f7ef380b8167e805ef1
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-12 23:12:29 +0600

    custom stepper  added

--- lib/presentation/widgets/custom_stepper.dart
+++ lib/presentation/widgets/custom_stepper.dart
@@ -1,13 +1,92 @@
 import 'package:flutter/material.dart';
 
-class CustomStepper extends StatelessWidget {
-  const CustomStepper({super.key});
+import '../utility/app_colors.dart';
+
+@immutable
+class CustomStepper extends StatefulWidget {
+  CustomStepper({
+    super.key,
+    required this.lowerLimit,
+    required this.upperLimit,
+    required this.stepValue,
+    required this.value,
+    required this.onChange,
+  });
+
+  final int lowerLimit;
+  final int upperLimit;
+  final int stepValue;
+  int value;
+  final Function(int) onChange;
 
   @override
+  _CustomStepperState createState() => _CustomStepperState();
+}
+
+class _CustomStepperState extends State<CustomStepper> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(25),
+            child: Container(
+              padding: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.circular(3)),
+              child: const Icon(
+                Icons.remove,
+                color: Colors.white,
+                size: 18,
+              ),
+            ),
+            onTap: () {
+              widget.value = widget.value == widget.lowerLimit
+                  ? widget.lowerLimit
+                  : widget.value -= widget.stepValue;
+              widget.onChange(widget.value);
+              setState(() {});
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              //'0${widget.value}',
+              '${widget.value < 10 ? '0${widget.value}' : widget.value}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          InkWell(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              padding: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.circular(3)),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 18,
+              ),
+            ),
+            onTap: () {
+              widget.value = widget.value == widget.upperLimit
+                  ? widget.upperLimit
+                  : widget.value += widget.stepValue;
+              widget.onChange(widget.value);
+              setState(() {});
+            },
+          ),
+        ],
       ),
     );
   }
