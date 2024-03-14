bcc142efadc31f988d12069ca6eb448c66eb0318
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-14 23:27:18 +0600

    Review UI screen,

--- lib/presentation/ui/screens/display_review_screen.dart
+++ lib/presentation/ui/screens/display_review_screen.dart
@@ -1,4 +1,6 @@
+import 'package:craftybay/presentation/ui/screens/create_review_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DisplayReviewScreen extends StatelessWidget {
   const DisplayReviewScreen({super.key});
@@ -6,9 +8,15 @@ class DisplayReviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Review'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Review'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Get.to(CreateReviewScreen());
+              },
+              child: const Icon(Icons.add)),
+        ));
   }
 }
