452f980d1220f35286f88a12e9ffedda15e5a1cb
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-03-17 22:07:15 +0600

    Review UI screen,

--- lib/presentation/ui/screens/create_review_screen.dart
+++ lib/presentation/ui/screens/create_review_screen.dart
@@ -1,6 +1,4 @@
-import 'package:craftybay/presentation/ui/screens/display_review_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class CreateReviewScreen extends StatefulWidget {
   const CreateReviewScreen({super.key});
@@ -14,51 +12,11 @@ class _CreateReviewScreenState extends State<CreateReviewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Create review'),
         backgroundColor: Colors.transparent,
-        leading: const BackButton(),
-        title: const Text('Create Review'),
+        leading: BackButton(),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(label: Text('First Name')),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(label: Text('Last Name')),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(label: Text('Write Review')),
-                maxLines: 20,
-                minLines: 10,
-              ),
-    SizedBox(
-    width: double.infinity,
-    child: Visibility(
-    visible: _reviewInProgress == false,
-    replacement: const Center(
-    child: CircularProgressIndicator()),
-    child: ElevatedButton(
-    onPressed: () {
-    _printDataFromField();
-    },
-    child: const Text('Submit'),
-            ],
-
-          ),
-        ),
-      ),
+      body: Column(),
     );
   }
 }
