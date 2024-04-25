16f5d18685b37414eccd8d45439fb55c5bba884f
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-04-25 23:46:51 +0600

    NetworkCaller added

---
+++ lib/data/service/network_caller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:http/http.dart';
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Response response = await get(Uri.parse(url));
+      log(response.statusCode.toString());
+      log(response.body);
+      if (response.statusCode == 200 &&
+          jsonDecode(response.body)['msg'] == 'success') {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else if (response.statusCode == 401) {
+        log("Token expired");
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
+  }
+}
