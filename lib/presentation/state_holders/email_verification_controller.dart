c1863df0fa87144342a20f1b125738564182fce9
Rokeya Yasmin <rokeyayasminmim17official@gmail.com>
2024-05-09 22:34:58 +0600

    Email verification controller has been modified

--- lib/presentation/state_holders/email_verification_controller.dart
+++ lib/presentation/state_holders/email_verification_controller.dart
@@ -1,60 +1,25 @@
+import 'package:craftybay/data/service/network_caller.dart';
+import 'package:craftybay/data/service/network_response.dart';
+import 'package:craftybay/presentation/utility/urls.dart';
 import 'package:get/get.dart';
 
-import '../../data/service/network_caller.dart';
-import '../../data/service/network_response.dart';
-import '../utility/urls.dart';
-
 class EmailVerificationController extends GetxController {
-  bool _emailVerificationInProgress = false;
-  String _message = '';
+  bool _emailVerificationInprogress = false;
+  final String _message = '';
 
-  bool get emailVerificationInProgress => _emailVerificationInProgress;
-
+  bool get emailVerificationInProgress => _emailVerificationInprogress;
   String get message => _message;
-
-  Future<bool> verifyEmail(String email) async {
-    _emailVerificationInProgress = true;
+  Future<bool> verifyemail(String email) async {
+    _emailVerificationInprogress = true;
     update();
     final NetworkResponse response =
         await NetworkCaller().getRequest(Urls.verifyEmail(email));
-    _emailVerificationInProgress = false;
-    _emailVerificationInProgress = false;
+    _emailVerificationInprogress = false;
     update();
     if (response.isSuccess) {
-      _message = response.responseJson?['data'] ?? '';
       return true;
     } else {
       return false;
     }
   }
 }
-//
-// import 'package:craftybay/presentation/utility/urls.dart';
-// import 'dart:html';
-// import 'package:craftybay/data/service/network_caller.dart';
-// import 'package:craftybay/data/service/network_response.dart';
-// import 'package:get/get.dart';
-//
-// class EmailVerificationController extends GetxController {
-//   bool get emailVerificationInprogress = false ;
-//   String message = '';
-//   bool get emailVerificationInProgress => _emailVerificationInProgress;
-//   String get message => _message;
-//   Future<bool> verifyemail(String email) async{
-//     _emailVerificationInProgress = true;
-//     update();
-//     final NetworkResponse response = await  NetworkCaller().getRequest(Urls.verifyEmail(email));
-//     _emailVerificationInProgress = false;
-//     update();
-//     if(
-//
-//     response.isSuccess
-//     ){
-//       return true;
-//     }
-//     else{
-//       return false;
-//     }
-//   }
-//
-// }
